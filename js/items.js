531204d7fbf2a770e324a1a0c102b10b298f3b0c
morronetlab <120627818+morronetlab@users.noreply.github.com>
2023-04-28 10:25:20 +0200

    Update items.js

--- js/items.js
+++ js/items.js
@@ -9,16 +9,16 @@ let items = [
     subtitle: "Som ny - brukt en gang",
     detail: "test test",
     secondaryImage: "https://brandonthegamedev.com/wp-content/uploads/2019/10/Tapestry-Board-Game.jpg",
-        amount: 300,
-        endTime: "2023-04-29T00:00:00+00:00"
+    amount: 300,
+    endTime: "2023-04-29T00:00:00+00:00",
   {
     primaryImage: "https://d3ur8vwbngpbs7.cloudfront.net/assets/editorial/2020/02/parks-board-game-cover.jpg",
     title: "Parks",
     subtitle: "Ny i plastikk",
     detail: "test test",
     secondaryImage: "https://d3ur8vwbngpbs7.cloudfront.net/assets/editorial/2020/02/parks-board-game-cover.jpg",
-      amount: 250,
-      endTime: "2023-04-29T00:00:00+00:00"
+    amount: 250,
+    endTime: "2023-04-29T00:00:00+00:00",
   },
   {
     primaryImage: "https://wheremeeplesmeet.files.wordpress.com/2019/02/img_4973.jpg?w=768",
@@ -27,7 +27,7 @@ let items = [
     detail: "test test",
     secondaryImage: "https://wheremeeplesmeet.files.wordpress.com/2019/02/img_4973.jpg?w=768",
       amount: 200,
-      endTime: "2023-04-29T00:00:00+00:00"
+      endTime: "2023-04-29T00:00:00+00:00",
   },
   {
     primaryImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO6YurAKjuE7SVvcngTB8b-GS9aNn366uHIw&usqp=CAU",
@@ -36,7 +36,7 @@ let items = [
     detail: "hmm!",
     secondaryImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO6YurAKjuE7SVvcngTB8b-GS9aNn366uHIw&usqp=CAU",
     amount: 200,
-    endTime: "2023-04-29T00:00:00+00:00"
+    endTime: "2023-04-29T00:00:00+00:00",
   }
 ];
 
